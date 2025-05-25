10a2cc67d52fd0547a669054d925504123403969
josh <joshcapitals07@gmail.com>
2025-05-25 12:31:41 -0400

    added SUB, MUL, DIV, print_stack and macros for instructions

--- jvm.c
+++ jvm.c
@@ -7,6 +7,9 @@ typedef enum {
     INST_PUSH,
     INST_POP,
     INST_ADD,
+    INST_SUB,
+    INST_MUL,
+    INST_DIV,
     INST_PRINT,
 } Inst_Set;
 
@@ -15,12 +18,18 @@ typedef struct {
     Inst_Set type;
 } Inst;
 
+#define DEF_INST_PUSH(x) {.type = INST_PUSH, .value = x}
+#define DEF_INST_POP() {.type = INST_POP}
+#define DEF_INST_ADD() {.type = INST_ADD}
+#define DEF_INST_SUB() {.type = INST_SUB}
+#define DEF_INST_MUL() {.type = INST_MUL}
+#define DEF_INST_DIV() {.type = INST_DIV}
+#define DEF_INST_PRINT() {.type = INST_PRINT}
+
 Inst program[] = {
-    {.type = INST_PUSH, .value = 15},
-    {.type = INST_PUSH, .value = 10},
-    {.type = INST_ADD},
-    {.type = INST_PRINT},
-    {.type = INST_PRINT},
+    DEF_INST_PUSH(5),
+    DEF_INST_PUSH(10),
+    DEF_INST_DIV(),
 };
 
 #define PROGRAM_SIZE (sizeof(program) / sizeof(program[0]))
@@ -45,6 +54,14 @@ int pop() {
     return stack[--stack_size];
 }
 
+void print_stack() {
+    printf("Stack: \n");
+    if (stack_size == 0) printf("EMPTY\n");
+    for (int i = 0; i < stack_size; i++) {
+	printf("%d. %d\n", i, stack[i]);
+    }
+}
+
 int main() {
     int a, b;
     for (size_t ip = 0; ip < PROGRAM_SIZE; ip++) {
@@ -60,10 +77,28 @@ int main() {
 		b = pop();
 		push(a + b);
 		break;
+	    case INST_SUB:
+		a = pop();
+		b = pop();
+		push(a - b);
+		break;
+	    case INST_MUL:
+		a = pop();
+		b = pop();
+		push(a * b);
+		break;
+	    case INST_DIV:
+		a = pop();
+		b = pop();
+		push(a / b);
+		break;
 	    case INST_PRINT:
 		printf("%d\n", pop());
 		break;
 	}
     }
+
+    print_stack();
+
     return 0;
 }
