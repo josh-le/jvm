2a2655f0547b084a805efa0d8b7e9a59121cf1f3
josh <joshcapitals07@gmail.com>
2025-05-25 09:43:41 -0400

    error handling for stack sizes and add instruction tested

--- jvm.c
+++ jvm.c
@@ -17,6 +17,9 @@ typedef struct {
 
 Inst program[] = {
     {.type = INST_PUSH, .value = 15},
+    {.type = INST_PUSH, .value = 10},
+    {.type = INST_ADD},
+    {.type = INST_PRINT},
     {.type = INST_PRINT},
 };
 
@@ -26,23 +29,36 @@ int stack[MAX_STACK_SIZE];
 int stack_size;
 
 void push(int value) {
+    if (stack_size >= MAX_STACK_SIZE) {
+	fprintf(stderr, "ERROR: Stack Overflow\n");
+	exit(1);
+    }
     stack[stack_size] = value;
     stack_size++;
 }
 
 int pop() {
+    if (stack_size <= 0) {
+	fprintf(stderr, "ERROR: Stack Underflow\n");
+	exit(1);
+    }
     return stack[--stack_size];
 }
 
 int main() {
+    int a, b;
     for (size_t ip = 0; ip < PROGRAM_SIZE; ip++) {
 	switch(program[ip].type) {
 	    case INST_PUSH:
 		push(program[ip].value);
 		break;
 	    case INST_POP:
+		pop();
 		break;
 	    case INST_ADD:
+		a = pop();
+		b = pop();
+		push(a + b);
 		break;
 	    case INST_PRINT:
 		printf("%d\n", pop());
